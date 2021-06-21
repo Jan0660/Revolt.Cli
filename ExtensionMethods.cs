468df71fa248c96d09d62b6caae876049fd15289
Jan0660 <jan0660@tutanota.com>
2021-06-21 16:09:13 +0200

    Initial code commit

---
+++ ExtensionMethods.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.Contracts;
+using System.Threading.Tasks;
+using Terminal.Gui;
+using SysCon = System.Console;
+
+namespace Revolt.Cli
+{
+    public static class ExtensionMethods
+    {
+        public static void SizeFix(this Window view, View parent)
+        {
+            // view.Width = SysCon.WindowWidth;
+            // view.Height = SysCon.WindowHeight - 1;
+            view.Width = parent.Width;
+            view.Height = parent.Height - 1;
+            Task.Delay(100).ContinueWith(_ =>
+            {
+                view.Width = Dim.Fill();
+                view.Height = Dim.Fill();
+            });
+        }
+        [Pure]
+        public static string Shorten(this string str, int length)
+            => str.Length > length ? str[..(length - 5)] + "(...)" : str;
+    }
+}
